d0c39a3915b747d45ea7408e7f87b4246795ddc9
Flamingo811 <129621208+Flamingo811@users.noreply.github.com>
2023-07-07 00:24:38 -0400

    2nd

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,7 @@
-
+/*    File Name: COMP229-S2023-MidTerm-301255100
+    Author Name: Ka Wah Lau
+     Student ID: 301255100
+   Web App name: flamingotesting2 */
 
 module.exports = {
   //local MongoDB deployment ->
